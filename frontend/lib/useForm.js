372efb404a548230dab205ecc90261f85af6eb77
Andrey Kruglik <isekaiguy@protonmail.com>
2022-07-13 12:53:39 +0700

    Form styling finished

--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -4,10 +4,10 @@ export default function useForm(initial = {}) {
   const [inputs, setInputs] = useState(initial);
 
   function handleChange(e) {
-    const { name, type, value } = e.target;
+    let { name, type, value } = e.target;
 
     if (type === 'file') {
-      value[0] = e.target.files;
+      [value] = e.target.files;
     }
 
     setInputs({ ...inputs, [name]: name === 'price' ? +value : value });
