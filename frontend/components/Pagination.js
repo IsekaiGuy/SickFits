4a57cc614dd9032eb5b9fa98a7695dbc6d5bb0e1
Andrey Kruglik <isekaiguy@protonmail.com>
2022-07-15 07:05:02 +0700

    Pagination routing added

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -23,16 +23,18 @@ export default function Pagination({ page }) {
   return (
     <PaginationStyles>
       <Head>
-        <title>Sick Fits - Page {page} of ___</title>
+        <title>
+          Sick Fits - Page {page} of {pageCount}
+        </title>
       </Head>
-      <Link href={`/products/${page - 1}`}>
+      <Link href={`/products/${+page - 1}`}>
         <a aria-disabled={page <= 1}>{'< Prev'}</a>
       </Link>
       <p>
         Page {page} of {pageCount}
       </p>
       <p>{count} Items total</p>
-      <Link href={`/products/${page - 1}`}>
+      <Link href={`/products/${+page + 1}`}>
         <a aria-disabled={page >= pageCount}>{'Next >'}</a>
       </Link>
     </PaginationStyles>
